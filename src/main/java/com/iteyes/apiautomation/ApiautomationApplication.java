aad45a82c008c9b1a265f5ca3b8d2dd451a45e5a
bkhun1214 <bkhun1214@gmail.com>
2021-04-23 17:56:21 +0900

    kihun

--- src/main/java/com/iteyes/apiautomation/ApiautomationApplication.java
+++ src/main/java/com/iteyes/apiautomation/ApiautomationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiautomationApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ApiautomationApplication.class, args);
     }
